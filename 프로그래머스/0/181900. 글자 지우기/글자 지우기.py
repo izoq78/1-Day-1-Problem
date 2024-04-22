2e5fe793bcd98b98bed04809e3fe27ca8c95639e
izoq78 <127970981+izoq78@users.noreply.github.com>
2024-04-22 13:02:29 +0900

    [level 0] Title: 글자 지우기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.py
@@ -0,0 +1,8 @@
+def solution(my_string, indices):
+    my_string = list(my_string)
+    indices.sort(reverse=True)
+    
+    for i in indices:
+        del my_string[i]
+        
+    return ''.join(my_string)
